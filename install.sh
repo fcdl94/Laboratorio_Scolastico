ad67f7de3f3722ca5e835eba49ec51b7e7f2e064
fcdl94 <f.cermelli94@gmail.com>
2018-08-07 16:36:53 +0200

    Added activation of TFTP

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install ansible git vim 
+yum install ansible git vim syslinux
 
 ln -s /var/lib/tftpboot /root/tftpboot
 ln -s /var/www/html /root/www
@@ -8,3 +8,7 @@ mkdir -p /root/www/ks/preseed
 cp edubuntu.seed /root/www/ks/preseed
 cp postseed.sh /root/www/ks/preseed
 cp -r tftpboot/* /root/tftpboot/
+
+/sbin/e-smith/db configuration getprop dnsmasq dhcp-boot
+/sbin/e-smith/db configuration setprop dnsmasq tftp-status enabled dhcp-boot pxelinux.0
+/sbin/e-smith/signal-event nethserver-dnsmasq-save
